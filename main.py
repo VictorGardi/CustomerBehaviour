f4075f2a842d9dd1f4ea1f72b9f66179e7b3ae8a
Anton Matsson <antonmatsson@Antons-MacBook-Pro.local>
2020-02-07 15:08:07 +0100

    updated main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from customer_behaviour.tools import dgm as dgm
 from customer_behaviour.tools.generate_data import User 
 import os
+import datetime
 import argparse
 import subprocess
 import numpy as np
+from os.path import join
 
 def main(case = 'discrete_events'):
     parser = argparse.ArgumentParser()
@@ -14,6 +16,8 @@ def main(case = 'discrete_events'):
     args = parser.parse_args()
 
     env = find_env(args.case, args.n_experts)
+
+    outdir = get_outdir(args.case, args.n_experts, n_categories=1)
 
     n_experts = 1
 
@@ -31,6 +35,26 @@ def main(case = 'discrete_events'):
              actions=np.array(demo_actions, dtype=object)) 
     command = ['python', 'customer_behaviour/algorithms/train_gym.py', args.algo, '--gpu', '-1', '--env', env, '--arch', 'FFSoftmax', '--steps', str(args.n_steps), '--load_demo', 'expert_trajectories.npz', '--update-interval', '128', '--entropy-coef' ,'0.01']
     s = subprocess.Popen(command)
+
+
+def get_outdir(case, n_experts, n_categories):
+
+    if not os.path.exists('results'): os.makedirs('results')
+
+    path = join('results', case)
+    if not os.path.exists(path): os.makedirs(path)
+
+    path = join(path, str(n_experts) + '_expert(s)')
+    if not os.path.exists(path): os.makedirs(path)
+
+    path = join(path, str(n_categories) + '_product(s)')
+    if not os.path.exists(path): os.makedirs(path)
+
+    time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    path = join(path, time)
+
+    return path
+
 
 def find_env(case, n_experts):
     if case == 'discrete_events':
