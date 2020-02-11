db57d29d3f66ed11b7e4b76e4477380a7a0af289
Anton Matsson <antonmatsson@Antons-MacBook-Pro.local>
2020-02-11 15:37:24 +0100

    updated gym

--- customer_behaviour/custom_gym/custom_gym/envs/discrete_buying_events.py
+++ customer_behaviour/custom_gym/custom_gym/envs/discrete_buying_events.py
@@ -14,13 +14,88 @@ def categorize_age(age):
     elif 70 <= age: return 1
 
 
+class SpaceSwitcher():
+    def __init__(self, n_historical_events):
+        self.n_historical_events = n_historical_events
+
+    def case_to_space(self, case):
+        method_name = '_case_' + str(case)
+        method = getattr(self, method_name)
+        return method()
+
+    def _case_1(self):
+        low = [0, 0] + self.n_historical_events * [0]
+        high = [1, 1] + self.n_historical_events * [1]
+        observation_space = spaces.Box(low=np.array(low), high=np.array(high), dtype=np.float32)
+
+        action_space = spaces.Discrete(2)
+
+        return observation_space, action_space
+
+
+class ActionSwitcher(self):
+    def __init__(self):
+        pass
+
+    def case_to_action(self, case, receipt):
+        method_name = '_case_' + str(case)
+        method = getattr(self, method_name)
+        return method(receipt)
+
+    def _case_1(self, receipt):
+        action = 1 if receipt[0] > 0 else 0  # We only consider the first item
+        return action
+
+
+class InitStateSwitcher(self):
+    def __init__(self, model, history):
+        self.model = model
+        self.history = history
+
+    def case_to_init_state(self):
+        method_name = '_case_' + str(case)
+        method = getattr(self, method_name)
+        return method()
+
+    def _case_1(self):
+        history = self.history[0, :]
+        history[history > 0] = 1  # We only consider the first item
+
+        initial_state = [self.model.sex, ategorize_age(self.model.age), *history]
+
+        return initial_state
+
+
+class StepSwitcher(self):
+    def __init__(self, model):
+        self.model = model
+
+    def case_to_step(self, state, action):
+        method_name = '_case_' + str(case)
+        method = getattr(self, method_name)
+        return method(state, action)
+
+    def _case_1(self, state, action):
+        history = state[2:]
+        new_history = [*history[1:], action]
+
+        new_state = [self.model.sex, categorize_age(self.model.age), *new_history]
+
+        return new_state
+
+
+###################################################################
+########## The code below should not need to be modified ##########
+###################################################################
+
+
 class DiscreteBuyingEvents(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         super(DiscreteBuyingEvents, self).__init__()
-        
+
         self.model = dgm.DGM()
 
         self.n_time_steps = 0
@@ -28,32 +103,24 @@ class DiscreteBuyingEvents(gym.Env):
         self.state = None
 
 
-    def initialize_environment(self, n_products, n_historical_events, episode_length, agent_seed=None):
+    def initialize_environment(self, case, n_historical_events, episode_length, agent_seed=None):
         # The implementaiton of this function depends on the chosen state representation
-
-        self.episode_length = episode_length
-
-        self.n_products = n_products
+        self.case = case
         self.n_historical_events = n_historical_events
-
+        self.episode_length = episode_length
         self.agent_seed = agent_seed
 
-        if self.n_products == 1:
-            # There are only two discrete actions: "buy something" or "do not buy something"
-            self.action_space = spaces.Discrete(2)
+        space_switcher = SpaceSwitcher(n_historical_events)
+        self.observation_space, self.action_space = space_switcher.case_to_space(case)
 
-            # The state consists of a customer's sex, age and purchase history
-            low = [0, 0] + self.n_historical_events * [0]
-            high = [1, 1] + self.n_historical_events * [1]
-            self.observation_space = spaces.Box(low=np.array(low), high=np.array(high), dtype=np.float32)
-        else:
-            # The action corresponds to a full receipt
-            raise NotImplementedError
+        self.step_switcher = StepSwitcher(self.model)
 
 
     def generate_expert_trajectories(self, n_experts, n_time_steps, out_dir, seed=True):
         states = []
         actions = []
+
+        action_switcher = ActionSwitcher()
 
         for i_expert in range(n_experts):
 
@@ -61,10 +128,14 @@ class DiscreteBuyingEvents(gym.Env):
             temp_actions = []
 
             self.model.spawn_new_customer(i_expert) if seed else model.spawn_new_customer()
-            # sample = self.model.sample(self.n_historical_events + n_time_steps)
+            sample = self.model.sample(self.n_historical_events + n_time_steps)
 
-            sample = np.zeros((6, self.n_historical_events + n_time_steps))
-
+<<<<<<< HEAD
+            # sample = np.zeros((6, self.n_historical_events + n_time_steps))
+            # ones = 6 * [1]
+            # indices = [int(i * (self.n_historical_events + n_time_steps)) for i in np.arange(0.1, 1.0, 0.1)]
+            # sample[:, indices] = ones
+=======
             #ones = [1, 1, 1, 1, 1, 1]
             ones = np.ones((6,int(0.05*(self.n_historical_events + n_time_steps))))
             print(int(0.05*(self.n_historical_events + n_time_steps)))
@@ -73,6 +144,7 @@ class DiscreteBuyingEvents(gym.Env):
                 j = int(i * (self.n_historical_events + n_time_steps))
                 sample[:, j:j+int(0.05 * (self.n_historical_events + n_time_steps))] = ones
                 
+>>>>>>> 0361000869450317cff7387b46333602b49dea05
 
             history = sample[:, :self.n_historical_events]        
             initial_state = self.initialize_state(history)
@@ -83,12 +155,8 @@ class DiscreteBuyingEvents(gym.Env):
 
             i = self.n_historical_events
             while i < sample.shape[1]:
-                if isinstance(self.action_space, spaces.Discrete):
-                    # There are only two discrete actions: "buy something" or "do not buy something"
-                    action = 1 if sample[0, i] > 0 else 0  # We only consider one item
-                elif isinstance(self.action_space, spaces.Box):
-                    raise NotImplementedError
-
+                receipt = sample[:, i]
+                action = action_switcher.case_to_action(self.case, receipt)
                 temp_actions.append(action)
 
                 if i == sample.shape[1] - 1:
@@ -110,16 +178,8 @@ class DiscreteBuyingEvents(gym.Env):
 
 
     def initialize_state(self, history):
-        # The implementaiton of this function depends on the chosen state representation
-
-        if self.n_products == 1:
-            history = np.sum(history, axis=0)
-            history[history > 0] = 1
-
-            initial_state = [self.model.sex, categorize_age(self.model.age), *history]
-        else:
-            raise NotImplementedError
-
+        init_state_swticher = InitStateSwticher(self.model, history)
+        initial_state = init_state_swticher.case_to_init_state(self.case)
         return initial_state
 
 
@@ -128,38 +188,23 @@ class DiscreteBuyingEvents(gym.Env):
 
 
     def step(self, action):
-        # The implementaiton of this function depends on the chosen state representation
-
-        if self.n_products == 1:
-            history = self.state[2:]
-            new_history = [*history[1:], action]
-
-            new_state = [self.model.sex, categorize_age(self.model.age), *new_history]
-
-            self.state = new_state
-
-            self.n_time_steps += 1
-
-            done = self.n_time_steps >= self.episode_length
-
-            reward = 0
-        else:
-            raise NotImplementedError
+        new_state = self.step_switcher.case_to_step(self.case, self.state, action)
+        
+        self.state = new_state
+        self.n_time_steps += 1
+        
+        done = self.n_time_steps >= self.episode_length
+        reward = 0
 
         return np.array(self.state), reward, done, {}
 
 
     def reset(self):
         # Reset the state of the environment to an initial state
-
         self.model.spawn_new_customer(self.agent_seed)
-
         sample = self.model.sample(self.n_historical_events)
-
         self.state = self.initialize_state(sample)
-
         self.n_time_steps = 0
-
         return np.array(self.state)
     
 
