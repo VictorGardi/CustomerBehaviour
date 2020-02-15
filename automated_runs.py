07b5d24ffeab0fe2278b85a577c8c167d96d7f7d
VictorGardi <victor.olssn@gmail.com>
2020-02-15 16:04:37 +0100

    changed automated_runs for case 3

--- automated_runs.py
+++ automated_runs.py
@@ -2,14 +2,28 @@ import subprocess
 
 calls = []
 """GLÖM INTE ATT ÄNDRA GPU INNAN DU KÖR PÅ VM!!!!"""
-call_1 = ['python', 'main.py', 'gail', '--gpu', '0', '--n_training_episodes', '10000', '--n_historical_events', '30', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '1', '--episode_length', '365', '--length_expert_TS', '365', '--seed_expert', 'True', '--agent_seed', '0']
-call_2 = ['python', 'main.py', 'gail', '--gpu', '0', '--n_training_episodes', '10000', '--n_historical_events', '60', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '1', '--episode_length', '365', '--length_expert_TS', '365', '--seed_expert', 'True', '--agent_seed', '0']
-call_3 = ['python', 'main.py', 'gail', '--gpu', '0', '--n_training_episodes', '10000', '--n_historical_events', '90', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '1', '--episode_length', '365', '--length_expert_TS', '365', '--seed_expert', 'True', '--agent_seed', '0']
-call_4 = ['python', 'main.py', 'gail', '--gpu', '0', '--n_training_episodes', '10000', '--n_historical_events', '120', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '1', '--episode_length', '365', '--length_expert_TS', '365', '--seed_expert', 'True', '--agent_seed', '0']
-call_5 = ['python', 'main.py', 'gail', '--gpu', '0', '--n_training_episodes', '10000', '--n_historical_events', '150', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '1', '--episode_length', '365', '--length_expert_TS', '365', '--seed_expert', 'True', '--agent_seed', '0']
+call_1 = ['python', 'main.py', 'airl', '--gpu', '0','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+ '--n_historical_events', '20', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '3', \
+  '--episode_length', '100', '--length_expert_TS', '100', '--seed_expert', 'True', '--agent_seed', '0']
+call_2 = ['python', 'main.py', 'airl', '--gpu', '0','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+ '--n_historical_events', '25', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '3', \
+  '--episode_length', '100', '--length_expert_TS', '100', '--seed_expert', 'True', '--agent_seed', '0']
+call_3 = ['python', 'main.py', 'airl', '--gpu', '0','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+ '--n_historical_events', '30', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '3', \
+  '--episode_length', '100', '--length_expert_TS', '100', '--seed_expert', 'True', '--agent_seed', '0']
+call_4 = ['python', 'main.py', 'airl', '--gpu', '0','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+ '--n_historical_events', '35', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '3', \
+  '--episode_length', '100', '--length_expert_TS', '100', '--seed_expert', 'True', '--agent_seed', '0']
+call_5 = ['python', 'main.py', 'airl', '--gpu', '0','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+ '--n_historical_events', '45', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '3', \
+  '--episode_length', '100', '--length_expert_TS', '100', '--seed_expert', 'True', '--agent_seed', '0']
 
+calls.append(call_1)
+calls.append(call_2)
+calls.append(call_3)
+calls.append(call_4)
+calls.append(call_5)
 
-calls.extend([[call_1], [call_2]])
 
 
 def terminate_instance():
