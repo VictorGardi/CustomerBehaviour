1c4cd84f6cb8cb1ad3b4d6e81d1eb4697881f800
Anton Matsson <antonmatsson@Antons-MacBook-Pro.local>
2020-02-26 10:49:14 +0100

    run_policy script

--- analyse_results.py
+++ analyse_results.py
@@ -11,21 +11,15 @@ from customer_behaviour.tools.cluster import Cluster
 from customer_behaviour.tools.result import Result
 from customer_behaviour.tools import dgm as dgm
 
-
-datetime = '2020-02-25_08-46-38'
-dir_path = '/results/gail/discrete_events/1_expert(s)/case_2/' + datetime
-
+dir_path = '/results/gail/discrete_events/1_expert(s)/case_2/tuesday_0225/hist64_exp256'
 
 def main():
     result = Case2(dir_path)
-    #result.plot_trajectories(n_trajectories = 3)
-    result.plot_clusters(n_dim = 3)
+    result.plot_trajectories(n_trajectories = 3)
+    #result.plot_clusters(n_dim = 3)
 	#result.plot_statistics()
 	#result.plot_cluster_data()
     
-	
-
-
 ############################
 ########## Case 3 ##########
 ############################
@@ -37,31 +31,10 @@ class Case3(Result):
     
     def __init__(self, dir_path):
         Result.__init__(self, dir_path)
-        self.n_historical_events = self.learner_states.shape[2] - 2
+        self.n_historical_events = self.learner_states.shape[2]
     
     def plot_trajectories(self, n_trajectories=None, expert=False):
-        if n_trajectories is None: 
-            n_trajectories = self.n_expert_trajectories if expert else self.n_learner_trajectories
-        
-        for i in range(n_trajectories):
-            if expert:
-                states = self.expert_states[i]
-                actions = self.expert_actions[i]
-            else:
-                states = self.learner_states[i]
-                actions = self.learner_actions[i]
-                cluster = Cluster(self.demo_features[i], self.expert_features)
-                self.mean_dist, self.min_dist, self.max_dist = cluster.get_dist_between_clusters()
-            
-            history = self.get_history(states[0])
-            
-            fig = self.plot(history, actions, expert)
-            
-            fig.suptitle('mean_dist: %.1f, min_dist: %.1f, max_dist: %.1f, var_expert_cluster: %.1f' 
-            	% (self.mean_dist, self.min_dist, self.max_dist, cluster.expert_within_SS))
-            
-        plt.show()
-        fig.tight_layout()      
+        raise NotImplementedError
         
     def get_history(self, initial_state):
         history = []
@@ -117,41 +90,66 @@ class Case1(Result):
     
     def __init__(self, dir_path):
         Result.__init__(self, dir_path) 
-        self.n_historical_events = self.learner_states.shape[2]
-        
-        # assert self.expert_states.shape[0] == 1
-        
-        self.n_learner_trajectories, self.episode_length, _ = self.learner_states.shape
         self.n_historical_events = self.learner_states.shape[2] - 2
-    
-    def plot_trajectories(self, n_trajectories=None):
-        expert_states = self.expert_states[0]
-        expert_actions = self.expert_actions[0]
-        expert_sex = expert_states[0][0]
-        expert_age = expert_states[0][1]
-        expert_history = expert_states[0][2:]
-        
-        if n_trajectories is None: n_trajectories = self.n_learner_trajectories
-        
-        for i in range(n_trajectories):
-            states = self.learner_states[i]
-            actions = self.learner_actions[i]
-            sex = states[0][0]
-            age = states[0][1]
-            history = states[0][2:]
             
-            expert_sex_str = 'female' if expert_sex == 1 else 'male'
-            sex_str = 'female' if sex == 1 else 'male'
-            text = 'Expert: {}, {} years old | Learner: {}, {} years old'.format(
-                expert_sex_str, get_age(expert_age), sex_str, get_age(age))
-                
-            fig = self.plot(expert_history, expert_actions, history, actions)  
-            fig.suptitle('Demonstration #{}'.format(i+1))
-            fig.text(0.5, 0, text, horizontalalignment='center', verticalalignment='center')
-                         
-        fig.tight_layout()
-        plt.show()
-
+    def plot_trajectories(self, n_trajectories=None):
+        raise NotImplementedError
 
 if __name__ == '__main__':
 	main()
+
+###########################
+########## Trash ##########
+###########################
+
+'''
+expert_states = self.expert_states[0]
+    expert_actions = self.expert_actions[0]
+    expert_sex = expert_states[0][0]
+    expert_age = expert_states[0][1]
+    expert_history = expert_states[0][2:]
+    
+    if n_trajectories is None: n_trajectories = self.n_learner_trajectories
+    
+    for i in range(n_trajectories):
+        states = self.learner_states[i]
+        actions = self.learner_actions[i]
+        sex = states[0][0]
+        age = states[0][1]
+        history = states[0][2:]
+        
+        expert_sex_str = 'female' if expert_sex == 1 else 'male'
+        sex_str = 'female' if sex == 1 else 'male'
+        text = 'Expert: {}, {} years old | Learner: {}, {} years old'.format(
+            expert_sex_str, get_age(expert_age), sex_str, get_age(age))
+            
+        fig = self.plot(expert_history, expert_actions, history, actions)  
+        fig.suptitle('Demonstration #{}'.format(i+1))
+        fig.text(0.5, 0, text, horizontalalignment='center', verticalalignment='center')
+                     
+    fig.tight_layout()
+    plt.show()
+
+if n_trajectories is None: 
+    n_trajectories = self.n_expert_trajectories if expert else self.n_learner_trajectories
+    
+    for i in range(n_trajectories):
+        if expert:
+            states = self.expert_states[i]
+            actions = self.expert_actions[i]
+        else:
+            states = self.learner_states[i]
+            actions = self.learner_actions[i]
+            cluster = Cluster(self.demo_features[i], self.expert_features)
+            self.mean_dist, self.min_dist, self.max_dist = cluster.get_dist_between_clusters()
+        
+        history = self.get_history(states[0])
+        
+        fig = self.plot(history, actions, expert)
+        
+        fig.suptitle('mean_dist: %.1f, min_dist: %.1f, max_dist: %.1f, var_expert_cluster: %.1f' 
+            % (self.mean_dist, self.min_dist, self.max_dist, cluster.expert_within_SS))
+        
+    plt.show()
+    fig.tight_layout()      
+'''
