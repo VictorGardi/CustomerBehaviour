d33fa800a21d1de43a3b5750fa28d82081c2b340
Anton Matsson <antonmatsson@Antons-MacBook-Pro.local>
2020-03-02 19:26:27 +0100

    import modified GAIL and Discriminator

--- customer_behaviour/algorithms/irl/gail/__init__.py
+++ customer_behaviour/algorithms/irl/gail/__init__.py
@@ -1,2 +1,4 @@
 from customer_behaviour.algorithms.irl.gail.discriminator import Discriminator
+from customer_behaviour.algorithms.irl.gail.discriminator_test import Discriminator2
 from customer_behaviour.algorithms.irl.gail.gail import GAIL
+from customer_behaviour.algorithms.irl.gail.gail_test import GAIL2
