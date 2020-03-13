0da096785c77df67cf0b9df868d4cba8e6c302f6
VictorGardi <victor.olssn@gmail.com>
2020-03-13 15:46:10 +0100

    modified automated runs

--- automated_runs.py
+++ automated_runs.py
@@ -2,39 +2,33 @@ import subprocess
 
 calls = []
 """GLÖM INTE ATT ÄNDRA GPU INNAN DU KÖR PÅ VM!!!!"""
-#call_1 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '1', '--n_training_episodes', '10000', \
-# '--n_historical_events', '32', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-#  '--episode_length', '128', '--length_expert_TS', '128', '--seed_expert', 'True', '--agent_seed', '0']
-#call_2 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '1', '--n_training_episodes', '10000', \
-# '--n_historical_events', '64', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-#  '--episode_length', '128', '--length_expert_TS', '128', '--seed_expert', 'True', '--agent_seed', '0']
-#call_3 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '1', '--n_training_episodes', '10000', \
-# '--n_historical_events', '96', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-#  '--episode_length', '128', '--length_expert_TS', '128', '--seed_expert', 'True', '--agent_seed', '0']
-#call_4 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '1', '--n_training_episodes', '10000', \
-# '--n_historical_events', '32', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-#  '--episode_length', '128', '--length_expert_TS', '256', '--seed_expert', 'True', '--agent_seed', '0']
-call_5 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '100', '--n_training_episodes', '30000', \
- '--n_historical_events', '64', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-  '--episode_length', '128', '--length_expert_TS', '256', '--seed_expert', 'True', '--agent_seed', '0', '--eval_episode_length', '256', '--eval_interval', '12800']
-call_6 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '100', '--n_training_episodes', '30000', \
- '--n_historical_events', '96', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-  '--episode_length', '128', '--length_expert_TS', '256', '--seed_expert', 'True', '--agent_seed', '0', '--eval_episode_length', '256', '--eval_interval', '12800']
-#call_7 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '1', '--n_training_episodes', '10000', \
-# '--n_historical_events', '32', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-#  '--episode_length', '128', '--length_expert_TS', '512', '--seed_expert', 'True', '--agent_seed', '0']
-call_8 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '100', '--n_training_episodes', '30000', \
- '--n_historical_events', '64', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-  '--episode_length', '128', '--length_expert_TS', '512', '--seed_expert', 'True', '--agent_seed', '0', '--eval_episode_length', '512', '--eval_interval', '12800']
-call_9 = ['python3', 'main.py', 'gail', '--update-interval', '2048', '--gpu', '0','--n_demos_per_expert', '100', '--n_training_episodes', '30000', \
- '--n_historical_events', '96', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '2', \
-  '--episode_length', '128', '--length_expert_TS', '512', '--seed_expert', 'True', '--agent_seed', '0', '--eval_episode_length', '512', '--eval_interval', '12800']
+call_1 = ['python3', 'main.py', 'airl', '--update-interval', '2048', '--gpu', '-1','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+'--n_historical_events', '30', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '21', '--episode_length', '1024',\
+'--length_expert_TS', '1024', '--seed_expert', 'True', '--seed_agent', 'True', '--eval_episode_length', '2048',\
+'--update-interval', '4096', '--eval_interval', '10240']
+call_2 = ['python3', 'main.py', 'airl', '--update-interval', '2048', '--gpu', '-1','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+'--n_historical_events', '60', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '21', '--episode_length', '1024',\
+'--length_expert_TS', '1024', '--seed_expert', 'True', '--seed_agent', 'True', '--eval_episode_length', '2048',\
+'--update-interval', '4096', '--eval_interval', '10240']
+call_3 = ['python3', 'main.py', 'airl', '--update-interval', '2048', '--gpu', '-1','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+'--n_historical_events', '90', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '21', '--episode_length', '1024',\
+'--length_expert_TS', '1024', '--seed_expert', 'True', '--seed_agent', 'True', '--eval_episode_length', '2048',\
+'--update-interval', '4096', '--eval_interval', '10240']
+call_4 = ['python3', 'main.py', 'airl', '--update-interval', '2048', '--gpu', '-1','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+'--n_historical_events', '120', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '21', '--episode_length', '1024',\
+'--length_expert_TS', '1024', '--seed_expert', 'True', '--seed_agent', 'True', '--eval_episode_length', '2048',\
+'--update-interval', '4096', '--eval_interval', '10240']
+call_5 = ['python3', 'main.py', 'airl', '--update-interval', '2048', '--gpu', '-1','--n_demos_per_expert', '20', '--n_training_episodes', '10000', \
+'--n_historical_events', '150', '--case', 'discrete_events', '--n_experts', '1', '--state_rep', '21', '--episode_length', '1024',\
+'--length_expert_TS', '1024', '--seed_expert', 'True', '--seed_agent', 'True', '--eval_episode_length', '2048',\
+'--update-interval', '4096', '--eval_interval', '10240']
+ 
 
+calls.append(call_1)
+calls.append(call_2)
+calls.append(call_3)
+calls.append(call_4)
 calls.append(call_5)
-calls.append(call_6)
-#calls.append(call_3)
-#calls.append(call_4)
-#calls.append(call_5)
 
 
 
@@ -46,7 +40,7 @@ def main():
     for call in calls:
         s = subprocess.Popen(call, stdout=subprocess.PIPE)
         output = s.stdout.readline()
-    terminate_instance()
+    #terminate_instance()
 
         
 
