12ba7fbf893196614b0a936bff90c9c847bad8b6
Anton Matsson <antonmatsson@Antons-MacBook-Pro.local>
2020-05-07 23:01:28 +0200

    ;)

--- report_material/report_material.py
+++ report_material/report_material.py
@@ -21,8 +21,9 @@ import results2 as res
 
 ##### ##### PARAMETERS ##### #####
 
-dir_path = 'gail_baseline'
+# dir_path = 'gail_baseline'
 # dir_path = 'airl_baseline'
+dir_path = 'dummy'
 
 sample_length = 10000
 n_new_customers = 50
@@ -55,6 +56,7 @@ customer_actions = np.array(customer_trajectories['actions'])
 customers = res.get_distribs(customer_states, customer_actions)
 expert_states = np.array(customer_trajectories['states'][:n_experts])
 expert_actions = np.array(customer_trajectories['actions'][:n_experts])
+experts = customers[:n_experts]
 avg_expert = pe.get_distrib(expert_states, expert_actions)
 
 # Sample agent data
@@ -71,49 +73,68 @@ def sample_agent_data(N, env, model, obs_normalizer):
     agent_actions = np.array(agent_actions)
     return agent_states, agent_actions
 # agent_states, agent_actions = sample_agent_data(n_experts, env, model, obs_normalizer)
-'''
+
 # Plot average distributions
-avg_agent = pe.get_distrib(agent_states, agent_actions)
+# avg_agent = pe.get_distrib(agent_states, agent_actions)
 
-print(wd(avg_expert, avg_agent))
+# print(wd(avg_expert, avg_agent))
 
-fig, ax = plt.subplots()
-data = {'Average expert': avg_expert, 'Average agent': avg_agent}
-pe.bar_plot(ax, data, colors=None, total_width=0.7)
-ax.set_xticks([], [])
-ax.set_ylabel('Probability')
-ax.set_title('AIRL')
-plt.show()
+# fig, ax = plt.subplots()
+# data = {'Average expert': avg_expert, 'Average agent': avg_agent}
+# pe.bar_plot(ax, data, colors=None, total_width=0.7, legend=False)
+# ax.set_xticks([], [])
+# ax.set_ylabel('Probability')
+# ax.set_title('AIRL')
+# plt.show()
 
 # Plot some individual customers
-agent2 = pe.get_distrib(agent_states[1], agent_actions[1])
-expert2 = pe.get_distrib(expert_states[1], expert_actions[1])
+# agent2 = pe.get_distrib(agent_states[1], agent_actions[1])
+# expert2 = pe.get_distrib(expert_states[1], expert_actions[1])
 
-print(wd(avg_expert, agent2))
-print(wd(expert2, agent2))
+# print(wd(avg_expert, agent2))
+# print(wd(expert2, agent2))
 
-fig, ax = plt.subplots()
-data = {'Agent 2': agent2, 'Expert 2': expert2, 'Average expert': avg_expert}
-pe.bar_plot(ax, data, colors=None, total_width=0.7)
-ax.set_xticks([], [])
-ax.set_ylabel('Probability')
-ax.set_title('AIRL')
-plt.show()
+# fig, ax = plt.subplots()
+# data = {'Agent 2': agent2, 'Expert 2': expert2, 'Average expert': avg_expert}
+# pe.bar_plot(ax, data, colors=None, total_width=0.7)
+# ax.set_xticks([], [])
+# ax.set_ylabel('Probability')
+# ax.set_title('AIRL')
+# plt.show()
 
-agent9 = pe.get_distrib(agent_states[8], agent_actions[8])
-expert9 = pe.get_distrib(expert_states[8], expert_actions[8])
+# agent9 = pe.get_distrib(agent_states[8], agent_actions[8])
+# expert9 = pe.get_distrib(expert_states[8], expert_actions[8])
 
-print(wd(avg_expert, agent9))
-print(wd(expert9, agent9))
+# print(wd(avg_expert, agent9))
+# print(wd(expert9, agent9))
 
-fig, ax = plt.subplots()
-data = {'Agent 9': agent9, 'Expert 9': expert9, 'Average expert': avg_expert}
-pe.bar_plot(ax, data, colors=None, total_width=0.7)
-ax.set_xticks([], [])
-ax.set_ylabel('Probability')
-ax.set_title('AIRL')
-plt.show()
-'''
+# fig, ax = plt.subplots()
+# data = {'Agent 9': agent9, 'Expert 9': expert9, 'Average expert': avg_expert}
+# pe.bar_plot(ax, data, colors=None, total_width=0.7)
+# ax.set_xticks([], [])
+# ax.set_ylabel('Probability')
+# ax.set_title('AIRL')
+# plt.show()
+
+# Plot heatmap
+# agents = res.get_distribs(agent_states, agent_actions)
+# distances = []
+# for i, a in enumerate(agents):
+#     temp = [wd(a, e) for e in experts]
+#     # temp.append(wd(a, avg_expert))
+#     distances.append(temp)
+
+# columns = ['Expert {}'.format(i + 1) for i in range(n_experts)]
+# columns.append('Avg. expert')
+# index = ['Agent {}'.format(i + 1) for i in range(n_experts)]
+
+# df = pd.DataFrame(distances, columns=columns, index=index)
+
+# fig, ax = plt.subplots()
+# fig.subplots_adjust(bottom=0.25)
+# sns.heatmap(df, cmap='BuPu', ax=ax, linewidth=1, cbar_kws={'label': 'Wasserstein distance'})
+# plt.show()
+
 ##### ##### BASELINE ##### #####
 
 model_dir_paths = [d for d in [x[0] for x in os.walk(dir_path)] if d.endswith('checkpoint')]
@@ -146,15 +167,51 @@ for mdp in model_dir_paths:
         data.append([n_steps, wd(a, avg_expert), 'Average expert'])
 
 df = pd.DataFrame(data, columns=['Number of training steps', 'Wasserstein distance', 'Comparison with'])
-df.to_csv('df_gail.csv', index=False)
+df.to_csv('df_dummies.csv', index=False)
 
-# df = pd.read_csv('df.csv')
+# df = pd.read_csv('df_airl.csv')
 # sns.set(style='darkgrid')
 # g = sns.relplot(x='Number of training steps', y='Wasserstein distance', hue='Comparison with', ci=95, kind='line', data=df)
 # g._legend.set_bbox_to_anchor([0.70, 0.85])
 # plt.savefig('gail_vs_train.eps', format='eps', transparent=True)
 # plt.show()
 
+##### ##### HEATMAPS ##### #####
+'''
+model_dir_paths = [d for d in [x[0] for x in os.walk(dir_path)] if d.endswith('checkpoint')]
+model_dir_paths.sort(key=res.get_key_from_path)
+
+columns = ['Expert {}'.format(i + 1) for i in range(n_experts)]
+index = ['Agent {}'.format(i + 1) for i in range(n_experts)]
+
+for mdp in model_dir_paths:
+    n_steps = res.get_key_from_path(mdp)
+
+    print('Processing model saved after %s' % n_steps)
+
+    if int(n_steps) <= 1000000: continue
+
+    if args['state_rep'] == 71:
+        env, model, obs_normalizer = pe.get_env_and_model(args, mdp, sample_length, n_experts_in_adam_basket=n_experts)
+    else:
+        env, model, obs_normalizer = pe.get_env_and_model(args, mdp, sample_length)
+
+    agent_states, agent_actions = sample_agent_data(n_experts, env, model, obs_normalizer)
+
+    agents = res.get_distribs(agent_states, agent_actions)
+    distances = []
+    for i, a in enumerate(agents):
+        temp = [wd(a, e) for e in experts]
+        distances.append(temp)
+
+    df = pd.DataFrame(distances, columns=columns, index=index)
+
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom=0.25)
+    sns.heatmap(df, cmap='BuPu', ax=ax, linewidth=1, cbar_kws={'label': 'Wasserstein distance'})
+    ax.set_title('%s training steps' % n_steps)
+    plt.show()
+'''
 ##### ##### EXPERT VISUALIZATION ##### #####
 '''
 experts = [pe.get_distrib(s, a) for s, a in zip(expert_states, expert_actions)]
